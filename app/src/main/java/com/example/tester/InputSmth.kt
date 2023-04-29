17a523c06a581450bc1cc548d09adbb4e49962e3
Suntrie <inga.egorova@waveaccess.ru>
2023-04-30 01:59:44 +0200

    POM version

--- app/src/main/java/com/example/tester/InputSmth.kt
+++ app/src/main/java/com/example/tester/InputSmth.kt
@@ -1,22 +1,27 @@
 package com.example.tester
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tester.ui.theme.TesterTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
-class MainActivity : ComponentActivity() {
+class InputSmth : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,33 +31,33 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android-T")
+                    Greeting2("Android")
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    Column() {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-        Button(onClick = {
-            context.startActivity(Intent(context, InputSmth::class.java))
-        }) {
-            Text(text = "Click me")
-        }
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    var text by remember {
+        mutableStateOf(TextFieldValue(""))
     }
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText }
+    )
+    Text(
+        text = "Bar",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview2() {
     TesterTheme {
-        Greeting("Android")
+        Greeting2("Android")
     }
 }
